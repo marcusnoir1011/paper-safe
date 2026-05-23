b05bbfc05a8d7b02ed067bef46cc13f0913a7630
Khun Min Myat <minmyatz617@gmail.com>
2026-05-24 08:54:45 +0900

    config updated

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  experimental: {
-    serverComponentsExternalPackages: ["tesseract.js"],
-  },
+  serverExternalPackages: ["tesseract.js"],
 };
 
 export default nextConfig;
